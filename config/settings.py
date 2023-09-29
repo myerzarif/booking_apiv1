38a66b5bf8be0ee872d610d2d5715dafac956699
gitlab_dev <gitlab@gmail.com>
2023-09-29 19:42:41 +0330

    change redis host name

--- config/settings.py
+++ config/settings.py
@@ -386,5 +386,5 @@ STRIPE_PUBLISHABLE_KEY=os.environ.get("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET_KEY=os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_BASE_URL=os.environ.get("STRIPE_BASE_URL")
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_HOST = os.environ.get('REDIS_HOST', 'booking_redis')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
