de2f73cd234e2448c0a3fd2bc31e636540e305bf
gitlab_dev <gitlab@gmail.com>
2023-11-14 17:58:27 +0330

    change urls order

--- transaction/urls.py
+++ transaction/urls.py
@@ -13,8 +13,8 @@ app_name = "transaction"
 
 urlpatterns = [
     path('', TransactionView.as_view(), name='search_transaction'),
-    path('<str:pk>', TransactionDetailView.as_view(), name='detail_transaction'),
     path('reservation', ReservationView.as_view(), name='create_reservation'),
+    path('<str:pk>', TransactionDetailView.as_view(), name='detail_transaction'),
     path('reservation/<str:pk>', ReservationDetailView.as_view(), name='detail_reservation'),
     path('payment', PaymentView.as_view(), name='create_payment'),
     path('stripe/webhook', StripeWebhookView.as_view(), name='stripe_webhook'),
